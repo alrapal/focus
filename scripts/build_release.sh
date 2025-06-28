bcb5cad02d074c907451b5fcb74bf19cdd41989d
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2025-06-28 18:41:43 +0200

    [Refactoring] - Repository as a workspace and adapted CI --- - Created a workspace to allow unit testing of no-std modules - Placeholder for a known crate "hl_driver" - Added scripts and vs code task for easier `build` and `test` tasks. This is because the target cannot be fixed in the `.cargo/config.toml` file - For the same reason, target is explicit in the `rust_ci.yml` file - Changed execution to target only PR to main to save processing - Added a `unit_tests_ci.yml` workflow to run unit tests at every   commit, based on std toolchain.
    resolves #7
    relates #4

---
+++ scripts/build_release.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# To change if not default esp32 rust toolchain installation
+# Import the rust toolchain using default script location
+. $HOME/export-esp.sh
+
+# Build for esp32s3 target
+cargo build --release --target xtensa-esp32s3-none-elf
